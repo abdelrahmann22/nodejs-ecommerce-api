63e35cbeec621d0cf5f3ff9b36638849496fb6fb
abdelrahmann202 <abdelrahmannn202@gmail.com>
2025-06-10 02:15:22 +0300

    setting up

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
 dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/db.js");
+const categoryRoute = require("./routes/categoryRoute.js");
+
+// Connect with db
+dbConnection();
+
+// Express app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Our App V2");
-});
-
+// Middleware
+app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`node: ${process.env.NODE_ENV}`);
+}
+// Mount Routes
+app.use("/api/v1/categories", categoryRoute);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
